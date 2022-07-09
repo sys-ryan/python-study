2fd886198eac945c4cfcf6bfb5bf732e0ebc1cdb
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-07-10 00:07:01 +0900

    feat: NATO project filter wrong letters

--- NATO-alphabet-project/main.py
+++ NATO-alphabet-project/main.py
@@ -1,15 +1,18 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
-data = pandas.read_csv('nato_phonetic_alphabet.csv')
-# print(data)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+data = pandas.read_csv('nato_phonetic_alphabet.csv')
 
 table = {value.letter:value.code for (index, value) in data.iterrows()}
 
-n = input('enter name: ')
-n = n.upper()
+while True:
+    n = input('enter name: ')
+    n = n.upper()
 
-output_list = [table[letter] for letter in n]
-print(output_list)
+    try:
+        output_list = [table[letter] for letter in n]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(output_list)
+        break
